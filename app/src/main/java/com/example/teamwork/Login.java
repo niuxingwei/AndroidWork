1721bde7edfffba1a478fa606db1ba35d4697620
“牛兴炜” <1669250716@qq.com>
2019-07-17 19:01:41 +0800

    牛兴炜第四次更新

--- app/src/main/java/com/example/teamwork/Login.java
+++ app/src/main/java/com/example/teamwork/Login.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
     //    定义登录、注册、注销三变量
@@ -15,15 +18,18 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     private Button registerButton;
     private Button cancelButton;
     private Intent intent;
+//    定义输入的用户名和密码两个变量
+    private EditText loginAccount;
+    private EditText loginPwd;
 
     //        确定注销动作，返回桌面
     private DialogInterface.OnClickListener okButton = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
-            intent.setAction(Intent.ACTION_MAIN);//设置动作
-            intent.addCategory(Intent.CATEGORY_HOME);//设置动作种类
-            startActivity(intent);
-
+            Intent intent1= new Intent(Intent.ACTION_MAIN);
+            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent1.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent1);
         }
     };
     //    取消注销动作，留在当前的界面
@@ -46,6 +52,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         loginButton = findViewById(R.id.login);
         registerButton = findViewById(R.id.register);
         cancelButton = findViewById(R.id.login_btn_cancle);
+        loginAccount= findViewById(R.id.login_edit_account);
+        loginPwd = findViewById(R.id.login_edit_pwd);
 //    事件注册
         loginButton.setOnClickListener(this);
         registerButton.setOnClickListener(this);
@@ -57,13 +65,28 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     //     事件监听
     @Override
     public void onClick(View view) {
+//        获取用户名和密码输入的内容
+        String name = loginAccount.getText().toString();
+        String pwd = loginPwd.getText().toString();
 //        点击事件区分
         switch (view.getId()) {
 //            去往登录界面
             case R.id.login:
-                intent = new Intent(Login.this, UserCenter.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+//                获取输入框的信息进行下一步状态判断
+                if (name.length()<1||pwd.length()<1){
+                    Toast.makeText(Login.this, "请输入您的用户名或者密码",Toast.LENGTH_LONG).show();
+                }
+//                创建文件保存信息
+                else {
+                    /*
+                    后续检测代码
+
+                     */
+                    intent = new Intent(Login.this, UserCenter.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+                }
                 break;
 //                去往注册界面
             case R.id.register:
--- app/src/main/java/com/example/teamwork/Sign.java
+++ app/src/main/java/com/example/teamwork/Sign.java
@@ -3,9 +3,11 @@ package com.example.teamwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Sign extends AppCompatActivity implements View.OnClickListener
 {
@@ -13,6 +15,9 @@ public class Sign extends AppCompatActivity implements View.OnClickListener
     private Button SureButton;
     private Button CancleButton;
     private Intent intent;
+    private EditText signmUser_name;
+    private EditText signUser_pwd;
+    private EditText signRepet_pwd;
 //函数入口
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,9 @@ public class Sign extends AppCompatActivity implements View.OnClickListener
     public void init(){
         SureButton = findViewById(R.id.register_btn_sure);
         CancleButton = findViewById(R.id.register_btn_cancel);
+        signmUser_name = findViewById(R.id.resetpwd_edit_name);
+        signUser_pwd = findViewById(R.id.resetpwd_edit_pwd_new);
+        signRepet_pwd= findViewById(R.id.resetpwd_edit_pwd_old);
 //        事件注册
         SureButton.setOnClickListener(this);
         CancleButton.setOnClickListener(this);
@@ -32,12 +40,26 @@ public class Sign extends AppCompatActivity implements View.OnClickListener
 //      事件监听
     @Override
     public void onClick(View view) {
+        String name_sign = signmUser_name.getText().toString();
+        String pwdnew = signUser_pwd.getText().toString();
+        String pwdold = signRepet_pwd.getText().toString();
         switch (view.getId()){
 //            注册确定按钮事件操作，返回登录界面
             case R.id.register_btn_sure:
-                intent= new Intent(Sign.this,Login.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+//                判断密码是否正确
+//                密码正确则保存用户信息并且跳转登陆界面
+                if (pwdnew.equals(pwdold)){
+//                    创建文件存储信息
+                    SharedPreferences sh = getSharedPreferences("SavedData",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sh.edit();
+//                    使用put添加信息
+                    intent= new Intent(Sign.this,Login.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                }
+
+
+
                 break;
             case R.id.register_btn_cancel:
                 intent= new Intent(Sign.this,Login.class);
